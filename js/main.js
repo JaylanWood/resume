a87eecd79147cba92aaab0930c2adfb2efd17353
jaylanwu <jaylanwu@outlook.com>
2019-09-14 14:37:46 +0800

    sticky topNavBar

--- js/main.js
+++ js/main.js
@@ -8,8 +8,9 @@ portfolio2.onclick = function () {
 portfolio3.onclick = function () {
     portfolioBar.className = 'bar state-3';
 }
-
-//加载动画
-
 //会变的topNavBar
+window.onscroll = function (xxx) {
+    console.log(window.scrollY)
+    window.scrollY > 0 ? topNavBar.classList.add('sticky') : topNavBar.classList.remove('sticky')
+}
 //滚动到指定元素
