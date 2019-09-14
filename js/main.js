39d619cb07bf8135940a7a1e098cc0d5a97372a8
jaylanwu <jaylanwu@outlook.com>
2019-09-14 21:22:19 +0800

    submenu

--- js/main.js
+++ js/main.js
@@ -8,9 +8,6 @@ portfolio2.onclick = function () {
 portfolio3.onclick = function () {
     portfolioBar.className = 'bar state-3';
 }
-//会变的topNavBar
-window.onscroll = function (xxx) {
-    console.log(window.scrollY)
-    window.scrollY > 0 ? topNavBar.classList.add('sticky') : topNavBar.classList.remove('sticky')
-}
+
+
 //滚动到指定元素
