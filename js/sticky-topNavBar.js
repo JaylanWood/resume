6c9cbdeb7d28c8429e8c0bcfd22eb590d0353512
jaylanwu <jaylanwu@outlook.com>
2019-11-01 01:14:08 +0800

    this的变化

--- js/sticky-topNavBar.js
+++ js/sticky-topNavBar.js
@@ -1,10 +1,46 @@
 ! function () {
     var view = document.querySelector('#topNavBar')
-    var controller = function (view) {
-        // controller 控制 view
-        window.addEventListener('scroll', function (x) {
-            window.scrollY > 0 ? view.classList.add('sticky') : view.classList.remove('sticky')
-        })
+    var controller = {
+        view: null,
+        init: function (view) {
+            this.view = view // bindEvents()不能访问传进来的view，但是可以访问this.view
+            this.bindEvents()
+            // this.bindEvents.call(this) 
+        },
+        bindEvents: function () {
+            var view = this.view // this.view相当于一个中转站
+            window.addEventListener('scroll', function (x) {
+                if (window.scrollY > 0) {
+                    this.active()
+                } else {
+                    this.deactive()
+                }
+            }).bind(this) // 因为这是一个监听，所以this是用户滚动。要用bind()函数把bindEvents()传进来的this作为this
+        },
+        active: function () {
+            this.view.classList.add('sticky')
+        },
+        deactive: function () {
+            this.view.classList.remove('sticky')
+        }
     }
-    controller(view)
+    controller.init(view)
+    // controller.init.call(controller, view) 
+
+    /*
+    this的变化：
+    1.执行controller.init.call(controller, view)，把controller传进来作为this。
+    2.执行this.bindEvents.call(this)，相当于controller.bindEvents.call(controller)，于是把controller传进来作为this。
+    3.执行this.active()，因为它是在一个监听事件里，所以本来的this就是用户scroll。
+        但是我们要把这个this改为bindEvents()传进来的this，也就是controller。
+        所以添加.bind(this)把this改为bindEvents()传进来的this，也就是controller。
+    */
+
+    /*
+    view的变化：
+    controller.view一开始是null
+    执行controller.init(view)，controller.view由null变为传进来的topNavBar
+    执行this.bindEvents.call(this)，用一个新的view变量存储controller.view，也就是topNavBar
+    执行this.active()，view就是这个新的view变量，也就是topNavBar
+    */
 }.call()
