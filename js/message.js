e3ccb35deb31827a933bb19d4faa869fdd7f445b
jaylanwu <jaylanwu@outlook.com>
2019-11-04 11:15:45 +0800

    代码MVC化

--- js/message.js
+++ js/message.js
@@ -1,39 +1,68 @@
-// leancloud初始化
-AV.init({
-    appId: "mJuVhxO68GtStDWtrhFJUX8t-gzGzoHsz",
-    appKey: "heQIds6bW7KitjLNGrrYGYv8",
-});
-// 读取leancloud
-var query = new AV.Query('message');
-query.find()
-    .then(
-        function (message) {
-            let array = message.map((item) => item.attributes)
-            let messageList = document.querySelector('#messageList')
-            array.forEach((item) => {
-                let li = document.createElement('li')
-                li.innerText = `${item.name}:${item.content}`
-                messageList.append(li)
+! function () {
+    var view = document.querySelector('section.message')
+
+    var model = {
+        initAV: function () {
+            AV.init({
+                appId: "mJuVhxO68GtStDWtrhFJUX8t-gzGzoHsz",
+                appKey: "heQIds6bW7KitjLNGrrYGYv8",
+            });
+        },
+        fetch: function () {
+            var query = new AV.Query('message');
+            return query.find()
+        },
+    }
+
+    var controller = {
+        view: null,
+        model: null,
+        messageList: null,
+        init: function (view, model) {
+            this.view = view
+            this.model = model
+            this.model.initAV()
+            this.messageList = view.querySelector('#messageList')
+            this.form = view.querySelector('#postMessageForm')
+            this.loadMessage()
+            this.bindEvents()
+        },
+        loadMessage: function () {
+            this.model.fetch().then(
+                (message) => {
+                    let array = message.map((item) => item.attributes)
+                    array.forEach((item) => {
+                        let li = document.createElement('li')
+                        li.innerText = `${item.name}:${item.content}`
+                        this.messageList.append(li)
+                    })
+                }
+            )
+        },
+        bindEvents: function () {
+            this.form.addEventListener('submit', (e) => {
+                e.preventDefault()
+                this.saveMessage()
             })
-        }
-    )
-// 写入leancloud
-let myForm = document.querySelector('#postMessageForm')
-postMessageForm.addEventListener('submit', function (e) {
-    e.preventDefault()
-    let name = myForm.querySelector('input[name=name]').value
-    let content = myForm.querySelector('input[name=content]').value
-    var Message = AV.Object.extend('message');
-    var message = new Message();
-    message.set('name', name);
-    message.set('content', content);
-    message.save()
-        .then(function (object) {
-            let li = document.createElement('li')
-            li.innerText = `${object.attributes.name}:${object.attributes.content}`
-            messageList.append(li)
-            myForm.querySelector('input[name=content]').value = ''
-            console.log('保存成功。')
-            console.log(object)
-        })
-})
+        },
+        saveMessage: function () {
+            let myForm = this.form
+            let content = myForm.querySelector('input[name=content]').value
+            let name = myForm.querySelector('input[name=name]').value
+            var Message = AV.Object.extend('message');
+            var message = new Message();
+            message.set('name', name);
+            message.set('content', content);
+            message.save().then(function (object) {
+                let li = document.createElement('li')
+                li.innerText = `${object.attributes.name}:${object.attributes.content}`
+                let messageList = document.querySelector('#messageList')
+                messageList.appendChild(li)
+                myForm.querySelector('input[name=content]').value = ''
+                console.log('保存成功。')
+                console.log(object)
+            })
+        },
+    }
+    controller.init(view, model)
+}.call()
