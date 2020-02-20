040ef12a32c3bd458d675c1e6d0d90e271fd05ad
jaylan <jaylanwu@outlook.com>
2020-02-20 22:02:13 +0800

    message的类化

--- js/7类化/message.js
+++ js/7类化/message.js
@@ -1,40 +1,16 @@
 // 留言板
 ! function () {
-    var view = document.querySelector('.message')
+    var view = View('.message')
 
-    var model = {
-        init: function () {
-            AV.init({
-                appId: "mJuVhxO68GtStDWtrhFJUX8t-gzGzoHsz",
-                appKey: "heQIds6bW7KitjLNGrrYGYv8",
-            });
-        },
-        fetch: function () {
-            var query = new AV.Query('message');
-            return query.find()
-        },
-        save: function (name, content) {
-            var Message = AV.Object.extend('message');
-            var message = new Message();
-            message.set('name', name);
-            message.set('content', content);
-            return message.save()
-        },
-    }
+    var model = Model({
+        resourceName: 'message'
+    })
 
-    var controller = {
-        view: null,
-        model: null,
-        messageList: null,
-        postMessageForm: null,
+    var controller = Controller({
         init: function (view, model) {
-            this.view = view
-            this.model = model
             this.messageList = this.view.querySelector('.messageList')
             this.postMessageForm = this.view.querySelector('.postMessageForm')
-            this.model.init()
             this.loadMessage()
-            this.bindEvents()
         },
         loadMessage: function () {
             this.model.fetch().then((message) => {
@@ -56,13 +32,16 @@
             var name = this.postMessageForm.querySelector('input[name=name]').value
             var content = this.postMessageForm.querySelector('input[name=content]').value
             // leancloud 提供的保存对象的代码
-            this.model.save(name, content).then((object) => {
+            this.model.save({
+                'name': name,
+                'content': content
+            }).then((object) => {
                 let li = document.createElement('li')
                 li.innerText = `${object.attributes.name}:${object.attributes.content}`
                 this.messageList.append(li)
                 this.postMessageForm.querySelector('input[name=content]').value = ''
             })
         },
-    }
+    })
     controller.init(view, model)
 }.call()
